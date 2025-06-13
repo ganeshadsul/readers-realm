b5f8db011be6479f0965e0c54720520466a3272a
Ganesh Adsul <ganeshadsul76@gmail.com>
2025-06-13 12:18:33 +0530

    Request validator changes to validate request payload

--- constants/errorMessages.js
+++ constants/errorMessages.js
@@ -4,6 +4,7 @@ const ERROR_MESSAGES = {
 	INVALID_CLIENT_CODE: "Invalid client code.",
 	API_ENDPOINT_NOT_FOUND: "API endpoint not found in suite.",
 	INVALID_REQUEST_TYPE: (method) => `Invalid request type. Use ${method}.`,
+	INVALID_REQUEST_PAYLOAD: "Request payload is not valid.",
 };
 
 module.exports = ERROR_MESSAGES;
--- middlewares/requestValidator.js
+++ middlewares/requestValidator.js
@@ -1,5 +1,6 @@
 const API_SUITE = require('../constants/apiSuite')
 const errorMessages = require('../constants/errorMessages')
+const AppError = require('../utils/AppError')
 
 const validateRequest = (req, res, next) => {
 	const { method, path } = req
@@ -8,12 +9,13 @@ const validateRequest = (req, res, next) => {
 
 	// Checking if API exists in the suite
 	if(!endPoint) {
-		return res.status(404).json({ message: errorMessages.API_ENDPOINT_NOT_FOUND })
+		return res.status(404).json({ status: 'fail', message: errorMessages.API_ENDPOINT_NOT_FOUND })
 	}
 
 	// Checking if API method is correct
 	if(endPoint.apiType !== method) {
 		return res.status(405).json({
+			status: 'fail',
 			message: errorMessages.INVALID_REQUEST_TYPE(endPoint.apiType)
 		})
 	}
@@ -21,11 +23,24 @@ const validateRequest = (req, res, next) => {
 	// Checking is client_code is valid
 	if(endPoint.clientCodeRequired && clientCode !== endPoint.clientCode) {
 		return res.status(403).json({
+			status: 'fail',
 			message: errorMessages.INVALID_CLIENT_CODE
 		})
 	}
 
-	// Validating request parameters
+	const payload = endPoint.payload || {}
+	// Validating request parameters and body
+	const requriedQueries = payload?.query?.required || []
+	const requriedBody = payload?.body?.required || []
+	const missingQueries = requriedQueries.filter((param) => !req.query.hasOwnProperty(param))
+	const missingbody = requriedBody.filter((param) => !req.body.hasOwnProperty(param))
+
+	if(missingQueries.length || missingbody.length) {
+		res.status(500).json({
+			status: 'fail',
+			message: errorMessages.INVALID_REQUEST_PAYLOAD
+		})
+	}
 	next()
 }
 
